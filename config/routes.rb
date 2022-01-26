d07debab9cc1106c97913a71b79981833a374dff
wawakokeshi <97097377+wawakokeshi@users.noreply.github.com>
2022-01-26 13:25:24 +0000

    [Update]3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/books' => 'books#index'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
   root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
