b25ae3109c4e998e4e98ce55e30078adae6675c4
wawakokeshi <97097377+wawakokeshi@users.noreply.github.com>
2022-01-29 08:49:06 +0000

    [Update]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
     if @book.save
      redirect_to book_path(@book.id)
     else
-    render :index
+     render :index
     end
   end
   
@@ -35,6 +35,6 @@ class BooksController < ApplicationController
   
   private
   def book_params
-    params.permit(:title,:body)
+    params.require(:book).permit(:title,:body)
   end  
 end
