be7f1af4a14bf2cfa08f7a104fc70dc7477ec028
wawakokeshi <97097377+wawakokeshi@users.noreply.github.com>
2022-01-29 08:04:44 +0000

    [Update]3-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,11 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end  
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
   
   private
   def book_params
